0084713efb82cfe6d41974dfd9bd1f4e634cef63
MH-Kniloy <web.kniloy@gmail.com>
2025-03-01 21:34:46 +0600

    on click comp btn activity log updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const taskBtnAll = document.querySelectorAll(".task-btn");
 const activeTask = document.getElementById("active-tasks");
 const totalTaskCompleted = document.getElementById("total-tasks-completed");
+const activityLog = document.getElementById("activity-log");
 
 let activeTaskCount = 6;
 
@@ -14,5 +15,11 @@ for (const taskBtn of taskBtnAll) {
     totalTaskCompleted.innerText = allTaskDone;
 
     e.target.setAttribute("disabled", true)
+
+    const div = document.createElement("div")
+    div.innerHTML = `<p class="p-[10px] rounded-[8px] bg-[#f4f7ff] opacity-70">
+     You have Complete The Task Add Dark Mode at 12:48:15 PM
+    </p>`;
+    activityLog.appendChild(div)
   });
 }
