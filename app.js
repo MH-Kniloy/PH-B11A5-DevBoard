806962f76db45c22d39a3977f3ba668e60bb2783
MH-Kniloy <web.kniloy@gmail.com>
2025-03-01 22:22:10 +0600

    current time added

--- app.js
+++ app.js
@@ -2,35 +2,38 @@ const taskBtnAll = document.querySelectorAll(".task-btn");
 const activeTask = document.getElementById("active-tasks");
 const totalTaskCompleted = document.getElementById("total-tasks-completed");
 const activityLog = document.getElementById("activity-log");
-const historyBtn = document.getElementById("history-btn")
-const currentDateHTML = document.getElementById("current-date")
-let currentDate = new Date().toDateString()
-currentDateHTML.innerText = currentDate
+const historyBtn = document.getElementById("history-btn");
+const currentDateHTML = document.getElementById("current-date");
+
+let currentDate = new Date().toDateString();
+currentDateHTML.innerText = currentDate;
+let currentTime = new Date().toLocaleTimeString()
+console.log(currentTime)
 let activeTaskCount = 6;
 
 let totalTaskCompletedCount = 23;
 
 for (const taskBtn of taskBtnAll) {
   taskBtn.addEventListener("click", function (e) {
-    alert("Board Updated Successfully")
+    alert("Board Updated Successfully");
     let remainingTask = (activeTaskCount -= 1);
     let allTaskDone = (totalTaskCompletedCount += 1);
     activeTask.innerText = remainingTask;
     totalTaskCompleted.innerText = allTaskDone;
 
-    e.target.setAttribute("disabled", true)
+    e.target.setAttribute("disabled", true);
 
-    const div = document.createElement("div")
+    const div = document.createElement("div");
     div.innerHTML = `<p class="p-[10px] rounded-[8px] bg-[#f4f7ff] opacity-70 mb-10">
-     You have Complete The Task Add Dark Mode at 12:48:15 PM
+     You have Complete The Task Add Dark Mode at ${currentTime}
     </p>`;
-    activityLog.appendChild(div)
-    if(activeTaskCount === 0){
-        alert("Congrats!!! You have completed all the current tasks")
+    activityLog.appendChild(div);
+    if (activeTaskCount === 0) {
+      alert("Congrats!!! You have completed all the current tasks");
     }
   });
 }
 
-historyBtn.addEventListener("click", function(){
-activityLog.innerHTML = ""
-})
+historyBtn.addEventListener("click", function () {
+  activityLog.innerHTML = "";
+});
